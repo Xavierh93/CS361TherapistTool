4b38fa8decf2d8fdfeaf28cae2eaf5277c8ee66a
KiratRan <33437032+KiratRan@users.noreply.github.com>
2018-11-30 12:38:38 -0800

    Fixed breaking error
    More info still doesn't actually display information, but it no longer throws an error

--- therapist.js
+++ therapist.js
@@ -16,18 +16,19 @@ module.exports = function(){
         });
     }
 
-    function getPerson(res, mysql, context, complete){
-        mysql.pool.query("SELECT fname, lname, specialty.name AS spec, sexualOrientation.name AS sOrien, religion.name AS rel, exp, location.city AS loc, mail FROM therapists \
+    function getPerson(res, mysql, context, id, complete){
+        var sql = "SELECT therID, fname, lname, specialty.name AS spec, sexualOrientation.name AS sOrien, religion.name AS rel, exp, location.city AS loc, mail FROM therapists\
         INNER JOIN specialty ON therapists.spec = specialty.specID \
         INNER JOIN location ON therapists.loc = location.locID \
         INNER JOIN sexualOrientation ON therapists.sOrien = sexualOrientation.sexID \
-        INNER JOIN religion ON therapists.rel = religion.relID WHERE therID = ?", 
-        function(error, results, fields){
+        INNER JOIN religion ON therapists.rel = religion.relID WHERE therID = ?";
+        var inserts = [id];
+        mysql.pool.query(sql, inserts, function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
             }
-            context.therapist = results;
+            context.therapist = results[0];
             complete();
         });
     }
@@ -54,7 +55,7 @@ module.exports = function(){
         callbackCount = 0;
         var context = {};
         var mysql = req.app.get('mysql');
-        getPerson(res, mysql, context, complete);
+        getPerson(res, mysql, context, req.params.id, complete);
         function complete(){
             callbackCount++;
             if(callbackCount >= 1){
